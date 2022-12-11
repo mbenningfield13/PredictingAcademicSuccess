93d58b71c443f23bdc8de85908c49e5e13199346
mbenningfield13 <53532360+mbenningfield13@users.noreply.github.com>
2022-12-11 14:14:31 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import pandas as pd
 import seaborn as sns
-from sklearn import linear_model
+import numpy as np
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPRegressor
 
-data = pd.read_csv("student-mat.csv", sep=';')
+data = pd.read_csv("student-mat.csv", sep=';')+pd.read_csv("student-mat.csv", sep=';')
 data['avgGrade'] = (data['G1'] + data['G2'] + data['G3'])/3
 sns.set_theme()
 
 #sns.heatmap(data.corr(), annot=True)
-#make an algorithm to determine which factors have the highest correlation with grades
 
-lmdata = []
-
-for i in data.columns:
-    if data[i].dtype == 'int64':
-        lmdata.append((i, abs(data[i].corr(data['avgGrade']))))
-
-sns.lmplot(data=data, x="Medu", y="avgGrade", ci=None)
-
-
-x = data.select_dtypes(include=['int64'])
+X = data[['absences', 'failures', 'Medu', "Fedu", 'studytime', 'age', 'traveltime', 'famrel', 'freetime', 'goout', 'Dalc', 'Walc', 'health']]
 y = data['avgGrade']
 
-regr = linear_model.LinearRegression()
-regr.fit(x, y)
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, y, test_size = 0.01, random_state = 0)
 
-print('Intercept: \n', regr.intercept_)
-print('Coefficients: \n', regr.coef_)
+regr = MLPRegressor(random_state=0, max_iter=100000, max_fun=100000, activation='relu', solver='lbfgs').fit(Xtrain, Ytrain)
+print(regr.score(Xtrain, Ytrain))
+
+#My data:
+print(regr.predict([[0, 0, 4, 4, 4, 17, 1, 5, 5, 3, 1, 3, 5],
+                    [0, 0, 4, 4, 4, 17, 1, 5, 5, 3, 1, 3, 5]])[0]/5)
+#My GPA: 4.4
+#Prediction: 4.50187536861295
 
 plt.show()
